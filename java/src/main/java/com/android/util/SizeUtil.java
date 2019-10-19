3d23710cc5bc865cd6b3c01f531ca0729765d4a9
xuzhb <xuzhb24@gmail.com>
2019-10-20 00:10:57 +0800

    工具类(Java)：添加尺寸信息单位转换工具SizeUtil

---
+++ java/src/main/java/com/android/util/SizeUtil.java
@@ -0,0 +1,31 @@
+package com.android.util;
+
+import com.android.application.BaseApplication;
+
+/**
+ * Created by xuzhb on 2019/10/19
+ * Desc:尺寸信息单位转换
+ */
+public class SizeUtil {
+
+    public static float px2dp(float pxValue) {
+        float density = BaseApplication.instance.getResources().getDisplayMetrics().density;
+        return pxValue / density + 0.5f;
+    }
+
+    public static float dp2px(float dpValue) {
+        float density = BaseApplication.instance.getResources().getDisplayMetrics().density;
+        return dpValue * density + 0.5f;
+    }
+
+    public static float px2sp(float pxValue) {
+        float scaledDensity = BaseApplication.instance.getResources().getDisplayMetrics().scaledDensity;
+        return pxValue / scaledDensity + 0.5f;
+    }
+
+    public static float sp2px(float spValue) {
+        float scaledDensity = BaseApplication.instance.getResources().getDisplayMetrics().scaledDensity;
+        return spValue * scaledDensity + 0.5f;
+    }
+
+}
