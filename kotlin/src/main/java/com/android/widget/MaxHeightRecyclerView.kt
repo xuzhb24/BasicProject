815801ec4d9757e4eb5cafc851f32a1f868fdde2
xuzhb <xuzhb24@gmail.com>
2019-10-28 19:48:42 +0800

    控件类(Kotlin)：添加MaxHeightRecyclerView

---
+++ kotlin/src/main/java/com/android/widget/MaxHeightRecyclerView.kt
@@ -0,0 +1,39 @@
+package com.android.widget
+
+import android.content.Context
+import android.support.annotation.AttrRes
+import android.support.v7.widget.RecyclerView
+import android.util.AttributeSet
+import com.android.basicproject.R
+
+/**
+ * Created by xuzhb on 2019/10/28
+ * Desc:重写RecyclerView，支持maxHeight属性
+ */
+class MaxHeightRecyclerView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, @AttrRes defStyleAttr: Int = 0
+) : RecyclerView(context, attrs, defStyleAttr) {
+
+    var maxHeight: Int = 0
+        set(value) {
+            field = value
+            if (maxHeight <= 0) {  //当值小于等于0时，maxHeight属性失效
+                requestLayout()    //重新布局
+            }
+        }
+
+    init {
+        val ta = context.obtainStyledAttributes(attrs, R.styleable.MaxHeightRecyclerView)
+        maxHeight = ta.getLayoutDimension(R.styleable.MaxHeightRecyclerView_maxHeight, 0)
+        ta.recycle()
+    }
+
+    override fun onMeasure(widthSpec: Int, heightSpec: Int) {
+        var heightSpec = heightSpec
+        if (maxHeight > 0) {
+            heightSpec = MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.AT_MOST)
+        }
+        super.onMeasure(widthSpec, heightSpec)
+    }
+
+}
