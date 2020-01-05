cd5a6218d955c7619fa4d1676831cc0807102bb9
xuzhb <xuzhb24@gmail.com>
2020-01-05 17:29:26 +0800

    工具类(Kotlin)：更新正则表达式工具RegexUtil

--- kotlin/src/main/java/com/android/util/RegexUtil.kt
+++ kotlin/src/main/java/com/android/util/RegexUtil.kt
@@ -1,5 +1,6 @@
 package com.android.util
 
+import java.lang.StringBuilder
 import java.util.regex.Pattern
 
 /**
@@ -8,34 +9,75 @@ import java.util.regex.Pattern
  */
 object RegexUtil {
 
-    //是否是数字
-    fun isDight(content: String): Boolean = isMatch("^[0-9]+\$", content)
+    //是否是整数，包括正数和负数
+    fun isInteger(content: String): Boolean {
+//        return isMatch("^[-\\+]?\\d+$", content)   //匹配"+1"格式
+        return isMatch("^-?\\d+$", content)  //不匹配"+1"格式
+    }
 
-    //是否只由数字、大小写字母组成
-    fun isLetterDigit(content: String): Boolean = isMatch("^[a-z0-9A-Z]+\$", content)
+    //是否是正整数
+    fun isPositiveInteger(content: String): Boolean = isMatch("\\d+", content)
+
+    //是否是小数
+    fun isDecimal(content: String): Boolean {
+//        return isMatch("^[-\\+]?\\d+[.]\\d+$", content)    //匹配"+1.2"格式
+        return isMatch("^-?\\d+[.]\\d+$", content)  //不匹配"+1.2"格式
+    }
+
+    //是否由数字或字母组成，包括大小写字母
+    fun isLetterOrDigit(content: String): Boolean = isMatch("^[a-zA-Z\\d]+$", content)
 
     //是否匹配正则表达式
     fun isMatch(regex: String, content: String): Boolean = Pattern.matches(regex, content)
 
-    //提取文本中的数字，如果存在多个，以空格分隔
-    fun extractDigit(content: String): String {
-        var result = ""
-        val pattern = Pattern.compile("\\d+")
+    //提取文本中的数字，包括整数和小数、正数和负数，如果存在多个，以指定的分隔符隔开，默认空格
+    fun extractNumber(content: String, delimiter: String = " "): String {
+        val sb = StringBuilder()
+        val pattern = Pattern.compile("(-)?(\\d+(\\.\\d+)?)")
         val matcher = pattern.matcher(content)
         while (matcher.find()) {
-            result += matcher.group() + " "
+            sb.append(matcher.group()).append(delimiter)
         }
-        return result.trim()
+        return sb.toString()
     }
 
     @JvmStatic
     fun main(args: Array<String>) {
-        println(isDight("1234"))
-        println(isDight("asd1234"))
-        println(isLetterDigit("1asd1234A12"))
-        println(isLetterDigit("1asd1234A12=-="))
-        println(extractDigit("1asd1234A12=-="))
-        println(extractDigit("一教楼101室"))
+        test("是否是整数", 1, "1234567890", "-1234567890", "+1234567890", "01", "-01", "1.2", "-1.2", "", "1-")
+        test("是否正整数", 2, "1234567890", "-1234567890", "+1234567890", "01", "-01", "1.2", "-1.2", "", "1-")
+        test(
+            "是否是小数",
+            3,
+            "12345.67890",
+            "-12345.67890",
+            "+12345.67890",
+            ".1",
+            "-.1",
+            "1.2",
+            "-1.2",
+            "",
+            "-123",
+            "-1.1.1"
+        )
+        test("数字或字母", 4, "1aA", "b2B", "Cc3", "4d", "dD", "D4", "567", "eee", "FFF", "su.12", "+ssdd12A")
+        test("提取数字", 5, "中文中文中文")
+        test("提取数字", 5, "纯整数123纯整数-456纯整数789纯整数-0012")
+        test("提取数字", 5, "纯小数-1.23纯小数4.56纯小数+7.89纯小数-00.123456789纯小数0.0.0纯小数.23纯小数4.纯小数")
+        test("提取数字", 5, "整数和小数12.34整数和小数-56整数和小数-78.90整数和小数12整数和小数12.8")
+    }
+
+    private fun test(tag: String, type: Int, vararg contents: String) {
+        print("$tag:  ")
+        for (content in contents) {
+            when (type) {
+                1 -> print("\"" + content + "\":" + isInteger(content) + "  ")
+                2 -> print("\"" + content + "\":" + isPositiveInteger(content) + "  ")
+                3 -> print("\"" + content + "\":" + isDecimal(content) + "  ")
+                4 -> print("\"" + content + "\":" + isLetterOrDigit(content) + "  ")
+                5 -> print("\"" + content + "\":  " + extractNumber(content, "|"))
+            }
+        }
+        println()
     }
 
 }
