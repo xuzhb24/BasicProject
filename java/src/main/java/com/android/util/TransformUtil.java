8d365d9c49bca41556e6ca076e774e2ac02feb71
xuzhb <xuzhb24@gmail.com>
2020-02-10 21:37:39 +0800

    工具类(Java)：更新转换工具TransformUtil

--- java/src/main/java/com/android/util/TransformUtil.java
+++ java/src/main/java/com/android/util/TransformUtil.java
@@ -6,12 +6,7 @@ package com.android.util;
  */
 public class TransformUtil {
 
-    /**
-     * byte转16进制
-     *
-     * @param b 需要进行转换的byte字节
-     * @return 转换后的Hex字符串
-     */
+    //byte转16进制
     public static String byte2Hex(byte b) {
         String hex = Integer.toHexString(b & 0xFF);
         if (hex.length() < 2) {
@@ -20,23 +15,13 @@ public class TransformUtil {
         return hex;
     }
 
-    /**
-     * 16进制转byte
-     *
-     * @param hex 待转换的Hex字符串
-     * @return 转换后的byte
-     */
+    //16进制转byte
     public static byte hex2Byte(String hex) {
         return (byte) Integer.parseInt(hex, 16);
     }
 
-    /**
-     * byte数组转16进制
-     *
-     * @param bytes 需要转换的byte数组
-     * @return 转换后的Hex字符串
-     */
-    public static String byteArray2Hex(byte[] bytes) {
+    //byte数组转16进制
+    public static String bytes2Hex(byte[] bytes) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < bytes.length; i++) {
             String hex = Integer.toHexString(bytes[i] & 0xFF);
@@ -48,13 +33,8 @@ public class TransformUtil {
         return sb.toString();
     }
 
-    /**
-     * 16进制转byte数组
-     *
-     * @param hex 待转换的Hex字符串
-     * @return 转换后的byte数组结果
-     */
-    public static byte[] hex2ByteArray(String hex) {
+    //16进制转byte数组
+    public static byte[] hex2Bytes(String hex) {
         int hexlen = hex.length();
         byte[] result;
         if (hexlen % 2 == 1) {  //奇数
@@ -75,9 +55,9 @@ public class TransformUtil {
     public static void main(String[] args) throws Exception {
         logTitlt("16进制和byte数组相互转换");
         String data = "abcdefg";
-        String hex = byteArray2Hex(data.getBytes("UTF-8"));
+        String hex = bytes2Hex(data.getBytes("UTF-8"));
         System.out.println(hex);
-        System.out.println(new String(hex2ByteArray(hex), "UTF-8"));
+        System.out.println(new String(hex2Bytes(hex), "UTF-8"));
     }
 
     private static void logTitlt(String title) {
