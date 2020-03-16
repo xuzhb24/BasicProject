ddbc5d13c7c5e5f4a2ccfc6cf0abf041ab3d271d
xuzhb <xuzhb24@gmail.com>
2020-03-16 23:16:01 +0800

    工具类(Java)：添加对象判空工具EmptyUtil

---
+++ java/src/main/java/com/android/util/EmptyUtil.java
@@ -0,0 +1,51 @@
+package com.android.util;
+
+import android.os.Build;
+import android.util.SparseArray;
+import android.util.SparseBooleanArray;
+import android.util.SparseIntArray;
+import android.util.SparseLongArray;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by xuzhb on 2020/3/16
+ * Desc:对象判空工具
+ */
+public class EmptyUtil {
+
+    //判断对象是否为空
+    public static boolean isEmpty(Object obj) {
+        if (obj == null) {
+            return true;
+        }
+        if (obj instanceof String && obj.toString().length() == 0) {
+            return true;
+        }
+        if (obj.getClass().isArray() && Array.getLength(obj) == 0) {
+            return true;
+        }
+        if (obj instanceof Collection && ((Collection) obj).isEmpty()) {
+            return true;
+        }
+        if (obj instanceof Map && ((Map) obj).isEmpty()) {
+            return true;
+        }
+        if (obj instanceof SparseArray && ((SparseArray) obj).size() == 0) {
+            return true;
+        }
+        if (obj instanceof SparseBooleanArray && ((SparseBooleanArray) obj).size() == 0) {
+            return true;
+        }
+        if (obj instanceof SparseIntArray && ((SparseIntArray) obj).size() == 0) {
+            return true;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            return obj instanceof SparseLongArray && ((SparseLongArray) obj).size() == 0;
+        }
+        return false;
+    }
+
+}
