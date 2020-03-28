742670a6d43ae8d84ae8cbce90000ae391c3a773
xuzhb <xuzhb24@gmail.com>
2020-03-28 23:45:32 +0800

    控件类(Kotlin)：更新扫码框架zxing

--- kotlin/src/main/java/com/android/frame/camera/zxing/AATest/ScanActivity.kt
+++ kotlin/src/main/java/com/android/frame/camera/zxing/AATest/ScanActivity.kt
@@ -1,11 +1,13 @@
 package com.android.frame.camera.zxing.AATest
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.animation.Animation
 import android.view.animation.DecelerateInterpolator
 import android.view.animation.TranslateAnimation
 import com.android.basicproject.R
 import com.android.frame.camera.zxing.ui.CaptureActivity
+import com.android.util.StatusBar.StatusBarUtil
 import kotlinx.android.synthetic.main.activity_scan.*
 
 /**
@@ -14,7 +16,13 @@ import kotlinx.android.synthetic.main.activity_scan.*
  */
 class ScanActivity : CaptureActivity() {
 
+    override fun initBar() {
+        StatusBarUtil.darkMode(this, Color.BLACK, 0f, false)
+    }
+
     override fun handleView(savedInstanceState: Bundle?) {
+        back_Iv.setOnClickListener { finish() }
+        album_tv.setOnClickListener { openAlbum() }
         startScanAnim()
     }
 
