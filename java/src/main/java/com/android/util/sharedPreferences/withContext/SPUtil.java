f91bceafc88732bcb641f76e14d0867ce6f54a87
xuzhb <xuzhb24@gmail.com>
2020-03-31 21:32:03 +0800

    工具类(Java)：更新SharedPreferences工具类SPUtil(带Context)

--- java/src/main/java/com/android/util/sharedPreferences/withContext/SPUtil.java
+++ java/src/main/java/com/android/util/sharedPreferences/withContext/SPUtil.java
@@ -2,6 +2,12 @@ package com.android.util.sharedPreferences.withContext;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.Nullable;
+
+import com.android.util.DataDueUtil;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by xuzhb on 2019/10/15
@@ -10,59 +16,102 @@ import android.content.SharedPreferences;
 public class SPUtil {
 
     //文件名
-    private static final String FILE_NAME = "shared_preferences";
+    private static final String SP_NAME = "share_data";
 
-    //保存数据
-    public static void put(Context context, String key, Object object) {
-        SharedPreferences sp = context.getApplicationContext().getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        if (object instanceof String) {
-            editor.putString(key, (String) object);
-        } else if (object instanceof Integer) {
-            editor.putInt(key, (Integer) object);
-        } else if (object instanceof Boolean) {
-            editor.putBoolean(key, (Boolean) object);
-        } else if (object instanceof Float) {
-            editor.putFloat(key, (Float) object);
-        } else if (object instanceof Long) {
-            editor.putLong(key, (Long) object);
-        }
-        editor.apply();
+    //保存字符串数据
+    public static void putString(Context context, String key, String value) {
+        getSharedPref(context).edit().putString(key, value).apply();
     }
 
-    //读取数据
-    public static Object get(Context context, String key, Object object) {
-        SharedPreferences sp = context.getApplicationContext().getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE);
-        if (object instanceof String) {
-            return sp.getString(key, (String) object);
-        } else if (object instanceof Integer) {
-            return sp.getInt(key, (Integer) object);
-        } else if (object instanceof Boolean) {
-            return sp.getBoolean(key, (Boolean) object);
-        } else if (object instanceof Float) {
-            return sp.getFloat(key, (Float) object);
-        } else if (object instanceof Long) {
-            return sp.getLong(key, (Long) object);
+    //保存字符串数据，saveTime：保存的时间，以秒为单位
+    public static void putString(Context context, String key, String value, int saveTime) {
+        getSharedPref(context).edit().putString(key, DataDueUtil.newStringWithTimeInfo(value, saveTime)).apply();
+    }
+
+    //读取字符串数据
+    public static String getString(Context context, String key, String defValue) {
+        String value = getSharedPref(context).getString(key, defValue);
+        if (!DataDueUtil.isDue(value)) {  //对于无时效性的数据和尚未过期的数据，isDue返回false
+            return DataDueUtil.clearTimeInfo(value);
+        } else {
+            remove(context, key);  //数据过期后移除原来的数据内容
+            return null;
         }
-        return null;
+    }
+
+    //保存字符串集合
+    public static void putStringSet(Context context, String key, @Nullable Set<String> value) {
+        getSharedPref(context).edit().putStringSet(key, value).apply();
+    }
+
+    //读取字符串集合
+    public static Set<String> getStringSet(Context context, String key, @Nullable Set<String> defValue) {
+        return getSharedPref(context).getStringSet(key, defValue);
+    }
+
+    //保存Int值
+    public static void putInt(Context context, String key, int value) {
+        getSharedPref(context).edit().putInt(key, value).apply();
+    }
+
+    //读取Int值
+    public static int getInt(Context context, String key, int defValue) {
+        return getSharedPref(context).getInt(key, defValue);
+    }
+
+    //保存Long值
+    public static void putLong(Context context, String key, long value) {
+        getSharedPref(context).edit().putLong(key, value).apply();
+    }
+
+    //读取Long值
+    public static long getLong(Context context, String key, long defValue) {
+        return getSharedPref(context).getLong(key, defValue);
+    }
+
+    //保存Boolean值
+    public static void putBoolean(Context context, String key, boolean value) {
+        getSharedPref(context).edit().putBoolean(key, value).apply();
+    }
+
+    //读取Boolean值
+    public static boolean getBoolean(Context context, String key, boolean defValue) {
+        return getSharedPref(context).getBoolean(key, defValue);
+    }
+
+    //保存Float值
+    public static void putFloat(Context context, String key, float value) {
+        getSharedPref(context).edit().putFloat(key, value).apply();
+    }
+
+    //读取Float值
+    public static float getFloat(Context context, String key, float defValue) {
+        return getSharedPref(context).getFloat(key, defValue);
+    }
+
+    //是否存在某个key
+    public static boolean contains(Context context, String key) {
+        return getSharedPref(context).contains(key);
+    }
+
+    //获取所有键值对
+    public static Map<String, ?> getAll(Context context) {
+        return getSharedPref(context).getAll();
     }
 
     //移除某个key值对应的值
     public static void remove(Context context, String key) {
-        SharedPreferences sp = context.getApplicationContext().getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.remove(key);
-        editor.clear().apply();
+        getSharedPref(context).edit().remove(key).apply();
     }
 
     //清除所有数据
-    public static void clearAll(Context context) {
-        SharedPreferences sp = context.getApplicationContext().getSharedPreferences(FILE_NAME,
-                Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.clear().apply();
+    public static void clear(Context context) {
+        getSharedPref(context).edit().clear().apply();
+    }
+
+    //获取SharedPreferences的Editor对象
+    private static SharedPreferences getSharedPref(Context context) {
+        return context.getApplicationContext().getSharedPreferences(SP_NAME, Context.MODE_PRIVATE);
     }
 
 }
