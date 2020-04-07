21ac733070ab4814fb05bf2cdc5aa3c609e13c54
xuzhb <xuzhb24@gmail.com>
2020-04-07 22:47:03 +0800

    控件类(Kotlin)：添加自定义指示器IndicatorLayout和com.youth.banner:banner的使用

---
+++ kotlin/src/main/java/com/android/widget/IndicatorLayout.kt
@@ -0,0 +1,98 @@
+package com.android.widget
+
+import android.content.Context
+import android.support.annotation.AttrRes
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.LinearLayout
+import com.android.basicproject.R
+import com.android.util.SizeUtil
+
+/**
+ * Created by xuzhb on 2020/4/6
+ * Desc:指示器
+ */
+class IndicatorLayout @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null, @AttrRes defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    companion object {
+        val DEFAULT_SELECTED_WIDTH = SizeUtil.dp2px(20f).toInt()
+        val DEFAULT_SIZE = SizeUtil.dp2px(5f).toInt()
+        val DEFAULT_MARGIN = SizeUtil.dp2px(10f).toInt()
+    }
+
+    private var mIndicatorCount: Int = 0   //指示器个数
+    private var mCurrentPosition: Int = 0  //指示器当前位置
+    private val mIndicatorList: MutableList<ImageView> = mutableListOf()
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        var height = 0
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize
+        } else {
+            height = DEFAULT_SIZE
+            if (heightMode == MeasureSpec.AT_MOST) {
+                height = Math.min(height, heightSize)
+            }
+        }
+        setMeasuredDimension(measuredWidth, height)
+    }
+
+    //设置指示器个数，必须大于1才生效
+    fun setIndicatorCount(indicatorCount: Int) {
+        mIndicatorList.clear()
+        mIndicatorCount = indicatorCount
+        if (indicatorCount <= 1) {
+            return
+        }
+        for (i in 1..indicatorCount) {
+            val indicatorIv = ImageView(context)
+            if (i == 1) {
+                val params = LayoutParams(DEFAULT_SELECTED_WIDTH, DEFAULT_SIZE)
+                params.leftMargin = 0
+                indicatorIv.layoutParams = params
+                indicatorIv.setImageResource(R.drawable.shape_indicator_select)
+            } else {
+                val params = LayoutParams(DEFAULT_SIZE, DEFAULT_SIZE)
+                params.leftMargin = DEFAULT_MARGIN
+                indicatorIv.layoutParams = params
+                indicatorIv.setImageResource(R.drawable.shape_indicator_no_select)
+            }
+            addView(indicatorIv)
+            mIndicatorList.add(indicatorIv)
+        }
+    }
+
+    //设置指示器当前位置，下标从0开始
+    fun setCurrentPosition(position: Int) {
+        if (mIndicatorCount <= 1) {
+            return
+        }
+        mCurrentPosition = position
+        if (position < 0) {
+            mCurrentPosition = 0
+        }
+        if (position > mIndicatorList.size - 1) {
+            mCurrentPosition = mIndicatorList.size - 1
+        }
+        for (i in mIndicatorList.indices) {
+            val params = mIndicatorList.get(i).layoutParams as LayoutParams
+            if (i == mCurrentPosition) {
+                params.width = DEFAULT_SELECTED_WIDTH
+                mIndicatorList.get(i).layoutParams = params
+                mIndicatorList.get(i).setImageResource(R.drawable.shape_indicator_select)
+            } else {
+                params.width = DEFAULT_SIZE
+                mIndicatorList.get(i).layoutParams = params
+                mIndicatorList.get(i).setImageResource(R.drawable.shape_indicator_no_select)
+            }
+        }
+    }
+
+
+}
--- kotlin/src/main/java/com/android/widget/TestSingleWidgetActivity.kt
+++ kotlin/src/main/java/com/android/widget/TestSingleWidgetActivity.kt
@@ -1,9 +1,21 @@
 package com.android.widget
 
 import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.view.ViewPager
+import android.text.TextUtils
+import android.view.Gravity
+import android.widget.ImageView
 import com.android.basicproject.R
 import com.android.frame.mvc.BaseActivity
+import com.android.util.SizeUtil
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.youth.banner.BannerConfig
+import com.youth.banner.Transformer
+import com.youth.banner.loader.ImageLoader
 import kotlinx.android.synthetic.main.activity_test_single_widget.*
 
 /**
@@ -53,6 +65,51 @@ class TestSingleWidgetActivity : BaseActivity() {
             setOnTextClearListener {
                 showToast("文本被清空了")
             }
+        }
+        //轮播图和自定义指示器
+        val imageList: MutableList<String> = mutableListOf(
+            "http://img8.zol.com.cn/bbs/upload/23197/23196119.jpg",
+            "http://pic1.win4000.com/wallpaper/b/55597435bb036.jpg",
+            "http://attach.bbs.miui.com/forum/201306/23/110328s72xxse7lfis9fnd.jpg",
+            "http://attach.bbs.miui.com/forum/201304/25/195151szk8umd8or8fmfa5.jpg",
+            "http://img1.imgtn.bdimg.com/it/u=2391986294,913424495&fm=26&gp=0.jpg"
+        )
+        idicatorLayout.setIndicatorCount(imageList.size)
+        banner.setBannerStyle(BannerConfig.NOT_INDICATOR)  //设置banner样式，不显示指示器和标题
+//            .setBannerStyle(BannerConfig.CIRCLE_INDICATOR)
+//            .setIndicatorGravity(Gravity.CENTER)  //设置指示器位置
+            .setImageLoader(GlideImageLoader())  //设置图片加载器
+            .setBannerAnimation(Transformer.Default)  //设置banner动画效果
+            .setDelayTime(3000)  //设置轮播时间
+            .isAutoPlay(true)  //设置自动轮播，默认为true
+            .setImages(imageList)  //设置图片集合
+            .setOnBannerListener { position ->
+                //点击图片回调
+                showToast("第${position + 1}张图片")
+            }
+            .start()
+        banner.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(p0: Int) {
+
+            }
+
+            override fun onPageScrolled(p0: Int, p1: Float, p2: Int) {
+
+            }
+
+            override fun onPageSelected(p0: Int) {
+                idicatorLayout.setCurrentPosition(p0)
+            }
+
+        })
+    }
+
+    //Glide加载图片
+    private inner class GlideImageLoader : ImageLoader() {
+        override fun displayImage(context: Context?, path: Any, imageView: ImageView) {
+            Glide.with(this@TestSingleWidgetActivity).load(path as String)
+                .apply(RequestOptions.bitmapTransform(RoundedCorners(SizeUtil.dp2px(10f).toInt())))  //转换成圆角图片
+                .into(imageView)
         }
     }
 
