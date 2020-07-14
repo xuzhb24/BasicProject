000b096cdb38f7890dcb8eb1d220981b9ce0477f
xuzhb <xuzhb24@gmail.com>
2020-07-14 20:27:50 +0800

    工具类(Java)：添加快速点击检测工具CheckFastClickUtil

---
+++ java/src/main/java/com/android/util/CheckFastClickUtil.java
@@ -0,0 +1,25 @@
+package com.android.util;
+
+/**
+ * Created by xuzhb on 2020/7/14
+ * Desc:快速点击检测工具
+ */
+public class CheckFastClickUtil {
+
+    private static long mLastClickTime = 0;
+
+    public static boolean isFastClick() {
+        return isFastClick(400);
+    }
+
+    public static boolean isFastClick(long timeInterval) {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - mLastClickTime > timeInterval) {
+            mLastClickTime = currentTime;
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
