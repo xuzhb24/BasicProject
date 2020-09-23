783a5bc6ac89b086faca280e87e0ad8a4b8eb8af
xuzhb <xuzhb24@gmail.com>
2020-09-23 14:41:42 +0800

    工具类(Kotlin)：更新尺寸信息单位转换工具SizeUtil

--- kotlin/src/main/java/com/android/util/SizeUtil.kt
+++ kotlin/src/main/java/com/android/util/SizeUtil.kt
@@ -1,5 +1,6 @@
 package com.android.util
 
+import android.content.Context
 import com.android.base.BaseApplication
 
 /**
@@ -8,24 +9,40 @@ import com.android.base.BaseApplication
  */
 object SizeUtil {
 
-    fun px2dp(pxValue: Float): Float {
-        val density: Float = BaseApplication.instance.resources.displayMetrics.density
+    fun px2dp(pxValue: Float, context: Context = BaseApplication.instance): Float {
+        val density: Float = context.resources.displayMetrics.density
         return pxValue / density + 0.5f
     }
 
-    fun dp2px(dpValue: Float): Float {
-        val density: Float = BaseApplication.instance.resources.displayMetrics.density
+    fun dp2px(dpValue: Float, context: Context = BaseApplication.instance): Float {
+        val density: Float = context.resources.displayMetrics.density
         return dpValue * density + 0.5f
     }
 
-    fun px2sp(pxValue: Float): Float {
-        val scaledDensity: Float = BaseApplication.instance.resources.displayMetrics.scaledDensity
+    fun px2sp(pxValue: Float, context: Context = BaseApplication.instance): Float {
+        val scaledDensity: Float = context.resources.displayMetrics.scaledDensity
         return pxValue / scaledDensity + 0.5f
     }
 
-    fun sp2px(spValue: Float): Float {
-        val scaledDensity: Float = BaseApplication.instance.resources.displayMetrics.scaledDensity
+    fun sp2px(spValue: Float, context: Context = BaseApplication.instance): Float {
+        val scaledDensity: Float = context.resources.displayMetrics.scaledDensity
         return spValue * scaledDensity + 0.5f
+    }
+
+    fun px2dpInt(pxValue: Float, context: Context = BaseApplication.instance): Int {
+        return px2dp(pxValue, context).toInt()
+    }
+
+    fun dp2pxInt(dpValue: Float, context: Context = BaseApplication.instance): Int {
+        return dp2px(dpValue, context).toInt()
+    }
+
+    fun px2spInt(pxValue: Float, context: Context = BaseApplication.instance): Int {
+        return px2sp(pxValue, context).toInt()
+    }
+
+    fun sp2pxInt(spValue: Float, context: Context = BaseApplication.instance): Int {
+        return sp2px(spValue, context).toInt()
     }
 
 }
